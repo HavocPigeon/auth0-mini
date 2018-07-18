ab9c694690fb6ed46a819567804772497cf46ce4
HavocPigeon <speeas@gmail.com>
2018-07-18 15:06:41 -0700

    final commit

--- server/index.js
+++ server/index.js
@@ -19,56 +19,72 @@ app.use(express.static(`${__dirname}/../build`));
 
 
 app.get('/auth/callback', (req, res) => {
-  
-  
-  
+    
   // STEP 1.
   // Make an object called payload with the code recieved from the clientside, client_id, client_secret, grant_type, redirect_uri 
   // Hint: 'code' is recieved from client side as a query
   
   const payload = {
-    // client_id
-    // client_secret
-    // code
-    // grant_type 
-    // redirect_uri
+    client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    client_secret: process.env.AUTH0_CLIENT_SECRET,
+    code: req.query.code,
+    grant_type: "authorization_code",
+    redirect_uri: `http://${req.headers.host}/auth/callback`
   };
   
   
   // STEP 2.
   // Write a function that returns an axios POST with the payload as the body.
   function tradeCodeForAccessToken() {
-    
-    // code here...
-    
+    return axios.post(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload)
   }
   
   // STEP 3.
   // Write a function that accepts the access token as a parameter and returns an axios GET to Auth0 that passes the access token as a query.
-  function tradeAccessTokenForUserInfo() {
-    
-    // code here ...
-    
+  function tradeAccessTokenForUserInfo(response) {
+    const accessToken = response.data.access_token
+    return axios.get(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo?access_token=${accessToken}`)
   }
   
   
   // STEP 4.
   // Write a function that accepts the userInfo as a parameter and returns a block of code.
   // Your code should set session, check your database to see if user exists and return their info or if they don't exist, insert them into the database.
-  function storeUserInfoInDataBase() {
-    
-    //code here...
-    
+  function storeUserInfoInDataBase(response) {
+    console.log('-------------------', response.data);
+    const auth0id = response.data.sub;
+    return req.app.get('db').find_user_by_auth0_id(auth0id).then(users => {
+      if (users.length) {
+        const user = users[0];
+        req.session.user = user;
+        res.redirect('/');
+      } else {
+        const createUserData = {
+          auth0_id: auth0id,
+          email: response.data.email,
+          profile_name: response.data.name,
+          picture: response.data.picture,
+        }
+        return req.app.get('db').create_user(createUserData).then(newUsers => {
+          const user = newUsers[0];
+          req.session.user = user;
+          res.redirect('/');
+        })
+      }
+    })
   }
    
   // Final code; uncomment after completing steps 1-4 above.
   
-  // tradeCodeForAccessToken()
-  // .then(accessToken => tradeAccessTokenForUserInfo(accessToken))
-  // .then(userInfo => storeUserInfoInDataBase(userInfo));
-  // });
+  tradeCodeForAccessToken()
+  .then(accessToken => tradeAccessTokenForUserInfo(accessToken))
+  .then(userInfo => storeUserInfoInDataBase(userInfo))
+  .catch(error => {
+  console.log(error)
+  res.status(500).json({message: 'Server error'})
+})
   
-});
+})  
 
 app.post('/api/logout', (req, res) => {
   req.session.destroy();
@@ -91,7 +107,8 @@ app.get('/api/secure-data', checkLoggedIn, (req, res) => {
   res.json({ someSecureData: 123 });
 });
 
-const SERVER_PORT = process.env.SERVER_PORT || 3040;
+const SERVER_PORT = 3040;
 app.listen(SERVER_PORT, () => {
   console.log('Server listening on port ' + SERVER_PORT);
-});
+})
+
